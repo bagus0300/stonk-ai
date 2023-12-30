2d405b912a02e139714871a4413a4d004d6f1ef8
marvin9830 <asamieto965@gmail.com>
2023-12-30 00:19:53 -0800

    Added loading animation additional articles after initial load

--- components/Loader/Loading.tsx
+++ components/Loader/Loading.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
+import React from "react";
 
 const Loading = () => {
   return (
-    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <div className="border-t-8 border-red-400 border-solid rounded-full animate-spin w-12 h-12"></div>
-    </div>
+    <div className="border-t-8 border-red-400 border-solid rounded-full animate-spin w-12 h-12"></div>
   );
 };
 
--- components/News/NewsDisplay.tsx
+++ components/News/NewsDisplay.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect, Suspense } from "react";
+import { useState, useEffect } from "react";
 import { Article } from "@/types/Article";
 import Card from "@/components/News/Card";
 import Loading from "@/components/Loader/Loading";
@@ -8,10 +8,14 @@ const NewsDisplay = () => {
   const [articles, setArticles] = useState<Article[]>([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(true);
+  const [loadingMore, setLoadingMore] = useState(false);
 
   const loadArticles = async () => {
     try {
-      setLoading(true);
+      if (!loading) {
+        setLoadingMore(true);
+      }
+      
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_BASE_URL}/api/articles/${page}`
       );
@@ -22,18 +26,19 @@ const NewsDisplay = () => {
     } catch (error) {
       console.error("Error fetching data:", error);
     } finally {
-      setLoading(false);
+      setLoadingMore(false);
     }
   };
 
   useEffect(() => {
     loadArticles();
+    setLoading(false);
   }, []);
 
   return (
     <div className="max-w-screen-lg mx-auto mt-10 mb-20">
       {loading ? (
-        <div className="text-center mt-4">
+        <div className="fixed top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center mt-4 ">
           <Loading />
         </div>
       ) : (
@@ -55,13 +60,21 @@ const NewsDisplay = () => {
               />
             ))}
           </div>
-          <div className="flex justify-center mt-10">
-            <button
-              className={`border text-green-500 border-green-500 hover:text-white hover:bg-green-600 transform hover:scale-105 font-semibold py-2 px-4 rounded mt-4 inline-block transition duration-300 ease-in-out cursor-pointer`}
-              onClick={loadArticles}
-            >
-              Load More
-            </button>
+          <div className="flex justify-center">
+            {loadingMore ? (
+              <div className="mt-10">
+                <Loading />
+              </div>
+            ) : (
+              <button
+                className={
+                  "mt-10 border text-green-500 border-green-500 hover:text-white hover:bg-green-600 transform hover:scale-105 font-semibold py-2 px-4 rounded inline-block transition duration-300 ease-in-out cursor-pointer"
+                }
+                onClick={loadArticles}
+              >
+                Load More
+              </button>
+            )}
           </div>
         </>
       )}
