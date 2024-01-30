4ea725a11b13e04d9100db6816071bd73219b27a
marvin9830 <asamieto965@gmail.com>
2024-01-29 22:23:34 -0800

    Move page title and search queries above the loading spinner

--- components/News/NewsDisplay.tsx
+++ components/News/NewsDisplay.tsx
@@ -70,9 +70,11 @@ const NewsDisplay = () => {
     setLoading(true);
     setCursor(0);
     const data = await loadArticles();
-    setArticles(data.articles);
-    setCursor(data.cursor);
-    setLoading(false);
+    if (data) {
+      setArticles(data.articles);
+      setCursor(data.cursor);
+      setLoading(false);
+    }
   };
 
   const loadNextPageArticles = async () => {
@@ -89,76 +91,77 @@ const NewsDisplay = () => {
 
   return (
     <div className="max-w-screen-lg mx-auto mt-3 mb-20">
-      {loading ? (
-        <div className="fixed top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center mt-4 ">
-          <Loading />
+      <div className="mx-10">
+        <div className="font-bold text-5xl mb-2">News</div>
+        <div className="mt-3 text-xl">View the latest financial news</div>
+        <div className="border-b border-gray-400 mb-2m mt-3"></div>
+        <div className="mt-5 flex gap-3"></div>
+        <div className="flex flex-row items-center space-x-3">
+          <MultiSelectDropdown
+            originalOptions={tickerOptions}
+            selectedOptions={selectedTickers}
+            setSelectedOptions={setSelectedTickers}
+            handleSubmit={getNewlyFilteredArticles}
+          />
+          <SingleSelectDropdown
+            placeholder={"Sentiment"}
+            originalOptions={sentimentOptions}
+            selectedOption={selectedSentiment}
+            setSelectedOption={setSelectedSentiment}
+          />
+          <SingleSelectDropdown
+            placeholder={"Price Action"}
+            originalOptions={priceActionOptions}
+            selectedOption={selectedPriceAction}
+            setSelectedOption={setSelectedPriceAction}
+          />
         </div>
-      ) : (
-        <>
-          <div className="mx-10">
-            <div className="font-bold text-5xl mb-2">News</div>
-            <div className="mt-3 text-xl">View the latest financial news</div>
-            <div className="border-b border-gray-400 mb-2m mt-3"></div>
-            <div className="mt-5 flex gap-3">
-              <MultiSelectDropdown
-                originalOptions={tickerOptions}
-                selectedOptions={selectedTickers}
-                setSelectedOptions={setSelectedTickers}
-                handleSubmit={getNewlyFilteredArticles}
-              />
-              <SingleSelectDropdown
-                placeholder={"Sentiment"}
-                originalOptions={sentimentOptions}
-                selectedOption={selectedSentiment}
-                setSelectedOption={setSelectedSentiment}
-              />
-              <SingleSelectDropdown
-                placeholder={"Price Action"}
-                originalOptions={priceActionOptions}
-                selectedOption={selectedPriceAction}
-                setSelectedOption={setSelectedPriceAction}
-              />
+        <div />
+        {loading ? (
+          <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+            <Loading />
+          </div>
+        ) : (
+          <>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mt-7 mx-10">
+              {articles.map((article: Article, index: number) => (
+                <Card
+                  key={index}
+                  title={article.title}
+                  publication_datetime={article.publication_datetime}
+                  summary={article.summary}
+                  ticker={article.ticker}
+                  sentiment={article.sentiment}
+                  image_url={article.image_url}
+                  article_url={article.article_url}
+                  market_date={article.market_date}
+                  open_price={article.open_price}
+                  close_price={article.close_price}
+                />
+              ))}
             </div>
-          </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mt-5 mx-10">
-            {articles.map((article: Article, index: number) => (
-              <Card
-                key={index}
-                title={article.title}
-                publication_datetime={article.publication_datetime}
-                summary={article.summary}
-                ticker={article.ticker}
-                sentiment={article.sentiment}
-                image_url={article.image_url}
-                article_url={article.article_url}
-                market_date={article.market_date}
-                open_price={article.open_price}
-                close_price={article.close_price}
-              />
-            ))}
-          </div>
-
-          <div className="flex justify-center">
-            {loadingMore ? (
-              <div className="mt-10">
-                <Loading />
-              </div>
-            ) : articles.length != 0 ? (
-              <button
-                className={
-                  "mt-10 border text-green-500 border-green-500 hover:text-white hover:bg-green-600 transform hover:scale-105 font-semibold py-2 px-4 rounded inline-block transition duration-300 ease-in-out cursor-pointer"
-                }
-                onClick={loadNextPageArticles}
-              >
-                Load More
-              </button>
-            ) : (
-              <div>No Articles Available</div>
-            )}
-          </div>
-        </>
-      )}
+            <div className="flex justify-center">
+              {loadingMore ? (
+                <div className="mt-10">
+                  <Loading />
+                </div>
+              ) : articles.length != 0 ? (
+                <button
+                  className={
+                    "mt-10 border text-green-500 border-green-500 hover:text-white hover:bg-green-600 transform hover:scale-105 font-semibold py-2 px-4 rounded inline-block transition duration-300 ease-in-out cursor-pointer"
+                  }
+                  onClick={loadNextPageArticles}
+                >
+                  Load More
+                </button>
+              ) : (
+                <div>No Articles Available</div>
+              )}
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
