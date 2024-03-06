01dcb0d11a9b49952a11f62c3805a308e0419607
marvin9830 <asamieto965@gmail.com>
2024-03-05 23:52:35 -0800

    feat: Install finnhub

--- src/app/stocks/page.tsx
+++ src/app/stocks/page.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import LineChart from '@/src/components/Stocks/LineChart'
 
+// const finnhub = require('finnhub');
+
+// const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+// api_key.apiKey = ""
+// const finnhubClient = new finnhub.DefaultApi()
+
+// finnhubClient.companyProfile2({'symbol': 'AAPL'}, (error: any, data: any, response: any) => {
+//   console.log(data)
+// });
+
 const page = () => {
   return (
     <LineChart symbol="AAPL"/>
