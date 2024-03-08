525e59e69f99525df05c02f2a6fa246cfefa1fa2
marvin9830 <asamieto965@gmail.com>
2024-03-07 21:07:22 -0800

    feat: Add stock api to get stock info by exchange

--- src/app/stocks/page.tsx
+++ src/app/stocks/page.tsx
@@ -1,23 +1,51 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+
 import LineChart from "@/src/components/Stocks/LineChart";
 import TickerCard from "@/src/components/Stocks/TickerCard";
-// import { finnhubClient } from "@/src/utils/FinnhubClient";
+
+interface StockInfo {
+  currency: string;
+  description: string;
+  displaySymbol: string;
+  figi: string;
+  mic: string;
+  symbol: string;
+  type: string;
+}
 
 const StocksPage = () => {
+  const [ stockInfo, setStockInfo ] = useState<StockInfo[] | null>(null);
+  const [ filteredStockInfo, setFilteredStockInfo ] = useState<StockInfo[] | null>(null);
+  const [ page, setPage ] = useState(0)
+
   useEffect(() => {
-    const finnhub = require("finnhub");
-    const api_key = finnhub.ApiClient.instance.authentications["api_key"];
-    api_key.apiKey = process.env.NEXT_PUBLIC_FINNHUB_KEY;
-    const finnhubClient = new finnhub.DefaultApi();
-    finnhubClient.stockSymbols("US", (error: any, data: any, response: any) => {
-      console.log(data);
-    });
+    const url = `https://finnhub.io/api/v1/stock/symbol?exchange=US&token=${process.env.NEXT_PUBLIC_FINNHUB_KEY}`;
+    axios.get(url)
+      .then(response => {
+        const commonStocks = response.data.filter((stock: StockInfo) => stock.type === 'Common Stock');
+        setStockInfo(commonStocks);
+      })
+      .catch(error => {
+        console.error('Error fetching company profile:', error);
+        setStockInfo(null);
+      });
   }, []);
+
+  useEffect(() => {
+    console.log(stockInfo);
+  }, [stockInfo]);
+  
+
   return (
     <>
-      <TickerCard ticker="TSLA" />
-      <LineChart ticker="AAPL" />
+      {stockInfo ? stockInfo
+        .slice(0, 10)
+        .map((stock) => (
+        <TickerCard key={stock.symbol} ticker={stock.symbol} />
+      )) : <p>Loading stocks...</p>}
+      {/* <LineChart ticker="AAPL" /> */}
     </>
   );
 };
