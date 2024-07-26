9826e7d24e5ca5e13e6fcf0162bdc4a51bc6d222
marvin9830 <asamieto965@gmail.com>
2024-07-26 08:41:55 +0700

    'fix:news'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,3 +3,5 @@ import NewsDisplay from "@/src/components/News/NewsPage";
 export default function Home() {
   return <NewsDisplay />;
 }
+//this is NewsPage components
+//this is NewsPage components
